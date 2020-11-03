aac6aee81711676968a5bb421bf303afd0c6f1da
Gaurav Balyan <gaurav.balyan24@gmail.com>
2020-11-03 23:25:34 +0530

    Error and Exception Handling added

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 (1) Have used MobX for state management
 
 (2) Have created snippets to prevent copy code but just use the snippets placed in .vscode/handlerSnippets.code-snippets
+
+(3) How to attach a debugger in .Net Core in VS Code
+
+(4) Exceptio and Error Handling
+
+(5) Adding Custom Middleware in .Net Core
+
+(6) API validation using Fluent validation
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,17 +1,25 @@
 import React, { Fragment } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Route, RouteComponentProps, withRouter } from 'react-router-dom';
+import {
+  Route,
+  RouteComponentProps,
+  Switch,
+  withRouter,
+} from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
+import { ToastContainer } from 'react-toastify';
 
 import NavBar from '../../features/nav/NavBar';
 import HomePage from '../../features/home/HomePage';
 import ActivityDashboard from '../../features/activities//dashboard/ActivityDashboard';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import NotFound from '../layout/NotFound';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
   return (
     <Fragment>
+      <ToastContainer position='bottom-right' />
       <Route exact path='/' component={HomePage} />
       <Route
         path={'/(.+)'}
@@ -19,13 +27,16 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
           <Fragment>
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
-              <Route exact path='/activities' component={ActivityDashboard} />
-              <Route path='/activities/:id' component={ActivityDetails} />
-              <Route
-                key={location.key}
-                path={['/createActivity', '/manage/:id']}
-                component={ActivityForm}
-              />
+              <Switch>
+                <Route exact path='/activities' component={ActivityDashboard} />
+                <Route path='/activities/:id' component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={['/createActivity', '/manage/:id']}
+                  component={ActivityForm}
+                />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </Fragment>
         )}
